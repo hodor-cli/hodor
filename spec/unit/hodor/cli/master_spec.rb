8296a79e2a068829934c36f18c59b0d919aee912
Ronald Hallman <dhallman@ulive.com>
2016-04-08 17:46:30 -0400

    Per Hal's suggestion, solving the problem in a different way. Instead of configuring commands in the clusters.yml file, make it easier to build commands that use Hodor's managed  secrets in a Rakefile, via it's "sh" command.

--- spec/unit/hodor/cli/master_spec.rb
+++ spec/unit/hodor/cli/master_spec.rb
@@ -27,27 +27,5 @@ module Hodor::Cli
         assert { result.stdout =~ /the_job_user/ }
       end
     end
-
-    context "Execute pre-configured command" do
-      include_context "hodor cli" do
-        let(:verbose) { false }
-        let(:run) { "master:exec :source_hive test.hql" }
-      end
-
-      before do
-        use_settings commands: {
-          source_hive: {
-            line: 'beeline -n test -u test_url -f',
-            ssh: false
-          }
-        }
-        expect(env).to receive(:run_local)
-          .with(/beeline -n test -u test_url -f test.hql/, ssh: false).once
-      end
-
-      it "should build expected command line from pre-configured command" do
-        assert { result.is_ok? }
-      end
-    end
   end
 end
