4ff3e7901314052b83ef9ed8a6bec6abb5123c8a
Hal Henningsmoen <hal.henningsmoen@gmail.com>
2016-06-13 17:54:36 -0600

    Fix unit tests

--- spec/unit/hodor/config/yml_source_spec.rb
+++ spec/unit/hodor/config/yml_source_spec.rb
@@ -1,17 +1,17 @@
-require 'hodor/config/yml_source'
+require 'hodor/config/source'
 
 module Hodor::Config
 
   describe YmlSource do
 
     describe "Required methods" do
-      subject { Source.instance_methods }
+      subject { YmlSource.instance_methods }
       it { should include :loader }
       it { should include :properties }
       it { should include :defaults }
       it { should include :name }
       it { should include :format_extension }
+      it { should include :config_hash }
     end
-
   end
 end
